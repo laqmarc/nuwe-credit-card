27cb2366a360e570c6aa0a908f1f8eec8e9c4618
laqmarc <laqmarc@gmail.com>
2022-12-08 17:27:05 +0100

    remove replace

--- app.js
+++ app.js
@@ -57,9 +57,7 @@ function formatString(e) {
     /^([0-1])([3-9])$/g, '0$1/$2' // 13 > 01/3
   ).replace(
     /^(0?[1-9]|1[0-2])([0-9]{2})$/g, '$1/$2' // 141 > 01/41
-  ).replace(
-    /^([0]+)\/|[0]+$/g, '0' // 0/ > 0 and 00 > 0
-  ).replace(
+   ).replace(
     /[^\d\/]|^[\/]*$/g, '' // To allow only digits and `/`
   ).replace(
     /\/\//g, '/' // Pre entering more than 1 `/`
